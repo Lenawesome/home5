284bc0789e3484fe13b69c41d71359100a2a5599
Lenawesome <nawilldominate@gmail.com>
2019-09-25 17:18:16 +0700

    Add slider

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        dots: false,
+        loop: true,
+        slideSpeed : 300,
+        paginationSpeed : 400,
+   
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+   
+  });
